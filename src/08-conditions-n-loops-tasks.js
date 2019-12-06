e113718eb1f90ef74822c98d8b511b3c725597f8
tanyashulha <tsulga6@gmail.com>
2019-12-06 11:00:58 +0300

    feat: add two cond and loop tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -135,8 +135,20 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  if (rect1.left > rect2.left + rect2.width) {
+    return false;
+  }
+  if (rect2.left > rect1.left + rect1.width) {
+    return false;
+  }
+  if (rect1.top + rect1.height < rect2.top) {
+    return false;
+  }
+  if (rect2.top + rect2.height < rect1.top) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -166,8 +178,12 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const p1 = point.x - circle.center.x;
+  const p2 = point.y - circle.center.y;
+  const square = Math.sqrt(p1 ** 2 + p2) ** 2;
+
+  return square < circle.radius;
 }
 
 
